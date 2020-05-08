a714d449985ebd47e5b54e396783573f9bdc7a02
Nirav Patel <niravkpatel28@gmail.com>
2020-05-08 16:50:11 +0530

    Updates

---
+++ precache-manifest.eb94fea3e3967bb8bb0bdb29378147b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "889682896712007faf8e3b01f7ccfe2c",
+    "url": "/react-singlepage-blog/index.html"
+  },
+  {
+    "revision": "803af0e3f460163d74e0",
+    "url": "/react-singlepage-blog/static/css/main.07545c26.chunk.css"
+  },
+  {
+    "revision": "5f574666e874b829cff4",
+    "url": "/react-singlepage-blog/static/js/2.5a91c970.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-singlepage-blog/static/js/2.5a91c970.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "803af0e3f460163d74e0",
+    "url": "/react-singlepage-blog/static/js/main.0a11d1ef.chunk.js"
+  },
+  {
+    "revision": "1d0bac540608223e5f62",
+    "url": "/react-singlepage-blog/static/js/runtime-main.ab3ddb51.js"
+  },
+  {
+    "revision": "498fb18a0c31dcdd3de5f845b8735420",
+    "url": "/react-singlepage-blog/static/media/banner.498fb18a.png"
+  }
+]);
